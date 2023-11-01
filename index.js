cc53e1ff11d6ffcf3c8335ba768114190e9cba90
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-02 01:27:17 +0600

    queried data from a id collection

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -45,6 +45,22 @@ async function run() {
       const totalProduct = await productCollection.countDocuments();
       res.send({ totalItem: totalProduct });
     });
+
+    app.post("/cart",async (req,res) => {
+      console.log(req.body)
+      const storedItems = req.body
+      const idWithObjectId=storedItems.map(id=>new ObjectId(id))
+      console.log(idWithObjectId)
+
+      const query={
+        _id : {$in:idWithObjectId}
+      }
+
+      const result = await productCollection.find(query).toArray()
+
+      res.send(result)
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
